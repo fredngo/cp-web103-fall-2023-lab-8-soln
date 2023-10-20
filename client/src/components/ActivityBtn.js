0b8b14052c84c3b665c683ede0b712d30b491eb3
Fred Ngo <fredngo@gmail.com>
2023-10-20 18:14:20 -0400

    Lab 6: Step 9: Update the number of votes for an activity

--- client/src/components/ActivityBtn.js
+++ client/src/components/ActivityBtn.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react';
 import './ActivityBtn.css'
-
 
 const ActivityBtn = (props) =>  {
 
+  const [num_votes, setNumVotes] = useState(props.num_votes)
+
+  const updateCount = () => {
+    const options = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({num_votes: num_votes + 1})
+    }
+    
+    fetch('/api/activities/' + props.id, options)
+    setNumVotes((num_votes) => num_votes + 1)
+  }
 
   return (
-          <button className="activityBtn" id={props.id} >{props.activity}</button>
-  );
+    <button className='activityBtn' id={props.id} onClick={updateCount}>
+      {props.activity} <br/> {'△ ' + num_votes + ' Upvotes' }
+    </button>
+  )
 
-};
+}
 
 export default ActivityBtn;
