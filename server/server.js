569c92b8717b7625b89fa5ab44b6248e683da46c
Fred Ngo <fredngo@gmail.com>
2023-11-03 12:18:37 -0400

    Lab 8: Step 2: Set up authentication middleware

--- server/server.js
+++ server/server.js
@@ -1,17 +1,44 @@
 import express from 'express'
 import cors from 'cors'
+
 import tripRoutes from './routes/trips.js'
 import activityRoutes from './routes/activities.js'
 import destinationRoutes from './routes/destinations.js'
 import tripDestinationRoutes from './routes/trips_destinations.js'
 
+import passport from 'passport'
+import session from 'express-session'
+import { GitHub } from './config/auth.js'
+
 const app = express()
 
+app.use(session({
+  secret: 'sq7taigbtwo2brby',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: 'GET,POST,PUT,DELETE,PATCH',
+  credentials: true
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(GitHub)
+
+passport.serializeUser((user, done) => {
+  done(null, user)
+})
+
+passport.deserializeUser((user, done) => {
+  done(null, user)
+})
 
 app.get('/', (req, res) => {
-  res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">✈️ On the Fly API</h1>')
+  res.redirect('http://localhost:3000')
 })
 
 app.use('/api/trips', tripRoutes)
